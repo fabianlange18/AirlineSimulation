Use initial state calculation metric, fix plotting

run.py
@@ -4,6 +4,8 @@ from calculation.dp_calculation import calculate_perfect_policy
 from calculation.adp_ql_calculation import adp_ql_calculation
 from calculation.dl_training import dl_training
 
+from util.calculate_init_value import InitialValueCalculator
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,57 +14,77 @@ cont_env = AirlineEnvironment(continuous_action_space=True)
 
 perfect_policy, perfect_value, perfect_reward = calculate_perfect_policy(discrete_env)
 
-episodes_array = [1, 5, 10, 50] #, 100, 500, 1000, 5000, 10000, 50000]
 
-adp  = []
-ql   = []
-dqn  = []
-a2c  = []
-td3  = []
-ppo  = []
-ddpg = []
-sac  = []
+init_value_calculator = InitialValueCalculator(discrete_env)
+
+models_array = ['adp', 'ql', 'dqn', 'a2c', 'td3', 'ppo', 'ddpg', 'sac']
+episodes_array = [1, 5, 10, 15] #, 100, 500, 1000, 5000, 10000, 50000]
+
+results = {model: {'r_means': [], 'r_std': [], 'v_means': [], 'v_std': []} for model in models_array}
+
+n_runs = 2
 
 for episodes in episodes_array:
-    adp_policy, _, adp_reward = adp_ql_calculation('adp', discrete_env, episodes, perfect_policy, perfect_value)
-    ql_policy, _, ql_reward = adp_ql_calculation('ql', discrete_env, episodes, perfect_policy, perfect_value)
-    dqn_policy, dqn_reward = dl_training('dqn', discrete_env, episodes, compare_policy=perfect_policy)
-    a2c_policy, a2c_reward = dl_training('a2c', discrete_env, episodes, compare_policy=perfect_policy)
-    td3_policy, td3_reward = dl_training('td3', cont_env, episodes, compare_policy=perfect_policy)
-    ppo_policy, ppo_reward = dl_training('ppo', discrete_env, episodes, compare_policy=perfect_policy)
-    ddpg_policy, ddpg_reward = dl_training('ddpg', cont_env, episodes, compare_policy=perfect_policy)
-    sac_policy, sac_reward = dl_training('sac', cont_env, episodes, compare_policy=perfect_policy)
 
-    adp.append(adp_reward)
-    ql.append(ql_reward)
-    dqn.append(dqn_reward)
-    a2c.append(a2c_reward)
-    td3.append(td3_reward)
-    ppo.append(ppo_reward)
-    ddpg.append(ddpg_reward)
-    sac.append(sac_reward)
+    intermediate_results = {model: {'r': [], 'v': []} for model in models_array}
+    
+    for _ in range(n_runs):
+
+        for model_name in models_array:
+
+            if model_name in ['adp', 'ql']:
+
+                policy, _, reward = adp_ql_calculation(model_name, discrete_env, episodes, perfect_policy, perfect_value)
+
+            elif model_name in ['ql', 'dqn', 'a2c', 'ppo']:
+
+                policy, reward = dl_training(model_name, discrete_env, episodes, compare_policy=perfect_policy)
+
+            else:
+
+                policy, reward = dl_training(model_name, cont_env, episodes, compare_policy=perfect_policy)
+
+            intermediate_results[model_name]['r'].append(reward)
+            intermediate_results[model_name]['v'].append(init_value_calculator.calculate_initial_value(policy))
+
+    for model_name in models_array:
+        results[model_name]['r_means'].append(np.mean(intermediate_results[model_name]['r']))
+        results[model_name]['r_std'].append(np.std(intermediate_results[model_name]['r']))
+        results[model_name]['v_means'].append(np.mean(intermediate_results[model_name]['v']))
+        results[model_name]['v_std'].append(np.std(intermediate_results[model_name]['v']))
 
 
-n = len(adp)
+n = len(episodes_array)
+k = len(models_array)
 r = np.arange(n)
-width = 0.08
+width = 0.8 / len(models_array)
 
-plt.bar(r, adp, width=width, label='ADP')
-plt.bar(r + width, ql, width=width, label='QL')
-plt.bar(r + 2 * width, dqn, width=width, label='DQN')
-plt.bar(r + 3 * width, a2c, width=width, label='A2C')
-plt.bar(r + 4 * width, td3, width=width, label='TD3')
-plt.bar(r + 5 * width, ppo, width=width, label='PPO')
-plt.bar(r + 6 * width, ddpg, width=width, label='DDPG')
-plt.bar(r + 7 * width, sac, width=width, label='SAC')
+for i, model_name in enumerate(models_array):
+    plt.bar(r + i * width, results[model_name]['r_means'], width=width, label = model_name, yerr = results[model_name]['r_std'])
 
 plt.axhline(y=perfect_reward, color='black', linestyle='--')
 
 plt.xlabel("Episodes")
 plt.ylabel("Reward")
-plt.title("Reward per Method and Training Steps")
+plt.title("Reward per Method and #Training Episodes")
 
-plt.xticks(r + 3.5 * width, episodes_array)
-plt.legend(loc='upper left')
+plt.xticks(r + (k-1)/2 * width, episodes_array)
+plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
 
-plt.savefig(f'./plots/summary')
+plt.savefig(f'./plots/summary_r', bbox_inches="tight")
+plt.close()
+
+for i, model_name in enumerate(models_array):
+    plt.bar(r + i * width, results[model_name]['v_means'], width=width, label = model_name, yerr = results[model_name]['v_std'])
+
+plt.axhline(y=perfect_value[*discrete_env.initial_state], color='black', linestyle='--')
+
+plt.xlabel("Episodes")
+plt.ylabel("Initial Values")
+plt.title("Initial Values per Method and #Training Episodes")
+
+plt.xticks(r + (k-1)/2 * width, episodes_array)
+plt.legend(loc='upper left', bbox_to_anchor=(1.04, 1))
+
+plt.savefig(f'./plots/summary_v', bbox_inches="tight")
+plt.close()

simulation.py
@@ -36,7 +36,9 @@ def simulation_run(policy, model_name, episodes):
     ax3.plot(reward_trajectory)
     ax3.set_title("Profits")
 
-    plt.savefig(f'./plots/simulations/{model_name}_{episodes}_r{int(np.sum(reward_trajectory))}')
+    plt.title(f"Total reward: {np.sum(reward_trajectory)}")
+
+    plt.savefig(f'./plots/simulations/{model_name}_{episodes}')
     plt.close()
     
     return np.sum(reward_trajectory)