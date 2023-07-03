63b6260ac043a5a79fa1cdd41f87213d189d43b1
ldreessen <leonard.dreessen@student.hpi.uni-potsdam.de>
2023-07-03 22:16:24 +0200

    updated customer behavior with continuous functions

--- airline_environment.py
+++ airline_environment.py
@@ -3,14 +3,16 @@ from gym.spaces.multi_discrete import MultiDiscrete
 from gym.spaces.discrete import Discrete
 from gym.spaces.box import Box
 
-#import wandb
+# import wandb
 
 import numpy as np
 from scipy.stats import multinomial
+from util.consumer_behavior import consumer_demand_timewise, consumer_demand_price_wise
+
 
 class AirlineEnvironment(gym.Env):
 
-    def __init__(self, continuous_action_space = True) -> None:
+    def __init__(self, continuous_action_space=True) -> None:
 
         # Observation Space
         self.booking_time = 10
@@ -19,9 +21,10 @@ class AirlineEnvironment(gym.Env):
 
         # Action Space
         self.max_price = 20
-        self.step_size = 1 # only relevant in discrete case
+        self.step_size = 1  # only relevant in discrete case
         self.continuous_action_space = continuous_action_space
-        self.action_space = Box(low = 0, high = self.max_price, shape = (1,)) if self.continuous_action_space else Discrete(int(self.max_price / self.step_size))
+        self.action_space = Box(low=0, high=self.max_price, shape=(1,)) if self.continuous_action_space else Discrete(
+            int(self.max_price / self.step_size))
         self.action_space_max = int(self.max_price / self.step_size)
 
         # Event Space
@@ -35,27 +38,19 @@ class AirlineEnvironment(gym.Env):
 
         self.reset()
 
-
     def transform_action(self, a):
         return a * self.step_size
 
     def get_p_dist(self, a, timestep):
-        # customer demand is price and time dependent
-        # based on 2 customer groups:
-        # family group: appears in the middle of the horizon and prefers cheap prices
-        # business group: appears at the very end of the horizon and is willing to pay more
-        # TODO: fix that indices could break
-        price_group_factor = [1, 1, 1.2, 1.5, 2, 1.5, 1.2, 1, 0.6, 0.3]
-        price_trend = (1 - (self.transform_action(a) * price_group_factor[timestep]) / self.max_price)
+        x_t = (10 / self.booking_time) * (timestep + 1)
+        x_p = (10 / self.booking_time) * timestep
 
+        price_trend = (1 - (self.transform_action(a) * consumer_demand_price_wise(x_p)) / self.max_price)
         time_sensitivity = 1.7
-        time_trend = [0.1, 0.106, 0.12, 0.123, 0.125, 0.25, 0.375, 0.4375, 0.46875, 0.5, 0.46875, 0.4375, 0.375, 0.25, 0.2, 0.22, 0.23, 0.25, 0.3, 0.5]
+        time_trend = consumer_demand_timewise(x_t) * time_sensitivity
 
-        t_index = int(20 / self.booking_time) * timestep
-        p = price_trend * (time_trend[t_index] * time_sensitivity)
+        p = price_trend * time_trend
 
-        # can get necessary?
-        # p = min(p, 1)
         return [p, 1 - p]
 
     def get_event_p(self, i, a, s):
